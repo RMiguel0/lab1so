3718f30a4dead0bc244bd5fe84bb5a2e1e531c02
RMiguel0 <mrivero@usm.cl>
2025-08-22 21:38:21 -0400

    khe

--- LAB1_Delgado_Rivero.cpp
+++ LAB1_Delgado_Rivero.cpp
@@ -54,7 +54,7 @@ void procesarArchivo(const fs::path& file) {
 
     } catch (exception& e) {
         cerr << "Error procesando " << file << ": " << e.what() << "\n";
-    }
+    } 
 }
 
 int main() {
