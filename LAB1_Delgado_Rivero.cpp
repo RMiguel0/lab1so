d6591845b10d60dc93869a62f5762bbf344fe40f
GaboDR <gaboo90685@gmail.com>
2025-08-22 22:29:28 -0400

    comentariios

--- LAB1_Delgado_Rivero.cpp
+++ LAB1_Delgado_Rivero.cpp
@@ -7,22 +7,26 @@
 
 
 using namespace std;
-
 namespace fs = std::filesystem;
 
+// variables globales de nombre de archivos
 int drivers = 0;
 int programas = 0;
 int link_library = 0;
 int pistas = 0;
 int basura = 0;
 
+/* se recibe el archivo, se establece la ruta y nombre para
+   el archivo organizado y se cambia la ubicacion con rename()
+
+   Mismo funcionamiento para las otras funciones
+*/
 void exe(const fs::path& file){
     programas++;
     fs::path carpeta = "Output/System/Programs";
     string nuevo_nombre = "programa" + to_string(programas) + ".exe";
     fs::path destino = carpeta / nuevo_nombre;
     rename(file,destino);
-    return;
 }
 
 void sys(const fs::path& file){
@@ -31,7 +35,6 @@ void sys(const fs::path& file){
     string nuevo_nombre = "drivers" + to_string(drivers) + ".sys";
     fs::path destino = carpeta / nuevo_nombre;
     rename(file,destino);
-    return;
 }
 
 void dll(const fs::path& file){
@@ -48,7 +51,6 @@ void pista(const fs::path& file){
     string nuevo_nombre = "pista" + to_string(pistas) + ".txt";
     fs::path destino = carpeta / nuevo_nombre;
     rename(file,destino);
-    return;
 }
 
 void trash(const fs::path& file){
@@ -57,14 +59,17 @@ void trash(const fs::path& file){
     string nuevo_nombre = "basura" + to_string(basura) + ".txt";
     fs::path destino = carpeta / nuevo_nombre;
     rename(file,destino);
-    return;
 }
 
 
+/* Se abre el archivo y se guarda en una variable tipo
+   string, luego se usa el metodo .find() para que llame
+   a la funcion adecuada
+*/
 void procesarArchivo(const fs::path& file) {
     try {
         ifstream in(file, ios::in | ios::binary);
-        if (!in) throw runtime_error("No se pudo abrir " + file.string());
+        if (!in) throw runtime_error("No se pudo abrir :( " + file.string());
 
         string contenido((istreambuf_iterator<char>(in)),
                                istreambuf_iterator<char>());
@@ -95,6 +100,13 @@ int main() {
 
     fs::create_directories("Temp");
     system("unzip -o archivos_de_prueba.zip -d Temp");
+
+    /* Se crea un vector de promesas con los archivos para
+       desplegar un procesamiento en paralelo con async, la
+       funcion de procesamiento y el archivo para despues ser
+       obtenidas con .get() y ser trabajadas como bloque de 
+       tamaño MAX_TAREAS
+    */
 
     const int MAX_TAREAS = 4;
     vector<future<void>> futures;
